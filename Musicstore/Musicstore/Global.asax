a8fa08d8e05340b8207249f617379a6bc81c6b9b
Filip Jovanovic <fil.93@hotmail.com>
2015-09-25 19:52:11 +0200

    First commit

---
+++ Musicstore/Musicstore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Musicstore.MvcApplication" Language="C#" %>
---
+++ Musicstore/Musicstore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Musicstore.Startup))]
+namespace Musicstore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
