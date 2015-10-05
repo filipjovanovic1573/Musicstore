3fee8546bede6f312585484ccf8d317822651477
Filip Jovanovic <fil.93@hotmail.com>
2015-10-05 18:11:01 +0200

    Minor changes

--- Musicstore/Musicstore/Controllers/AdminPanelController.cs
+++ Musicstore/Musicstore/Controllers/AdminPanelController.cs
@@ -1,15 +1,55 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Musicstore.Models;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Musicstore.ViewModels;
 
 namespace Musicstore.Controllers {
     public class AdminPanelController : Controller {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+        private ApplicationUserManager userManager;
+        public ApplicationUserManager UserManager {
+            get {
+                return userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set {
+                userManager = value;
+            }
+        }
+        
         // GET: AdminPanel
         public ActionResult Index() {
             return View();
+        }
+
+        public async Task<ActionResult> ListUsers() {
+            return View(await db.Users.ToListAsync());
+        }
+
+        public ActionResult UserDetails(string id) {
+            var user = UserManager.FindById(id);
+            var roles = UserManager.GetRoles(id).ToArray();
+            var detailsUser = new DetailsViewModel {
+                Id = user.Id,
+                UserName = user.UserName,
+                Email = user.Email,
+                EmailConfirmed = user.EmailConfirmed,
+                PhoneNumber = user.PhoneNumber,
+                PhoneNumberConfirmed = user.PhoneNumberConfirmed,
+                LockoutEnabled = user.LockoutEnabled,
+                LockoutEndDate = user.LockoutEndDateUtc,
+                TwoFactor = user.TwoFactorEnabled,
+                Roles = roles
+            };
+            return View(detailsUser);
         }
     }
 }
---
+++ Musicstore/Musicstore/ViewModels/AdminPanelViewModels.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Musicstore.ViewModels {
+    public class DetailsViewModel {
+        public string Id { get; set; }
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        [Display(Name = "Email confirmed")]
+        public bool EmailConfirmed { get; set; }
+        [Display(Name = "Phone")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "Phone confirmed")]
+        public bool PhoneNumberConfirmed { get; set; }
+        [Display(Name = "TwoFactor Auth")]
+        public bool TwoFactor{ get; set; }
+        [Display(Name = "Lockout")]
+        public bool LockoutEnabled { get; set; }
+        [Display(Name = "Locked until")]
+        public DateTime? LockoutEndDate { get; set; }
+        public string[] Roles { get; set; }
+    }
+}
